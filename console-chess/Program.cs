409872802aa11a7e864b6391b1e75d2554d160cf
João Vitor <bortoliero.10@hotmail.com>
2024-08-03 19:10:42 -0300

    Fazendo os primeiros testes de movimentação

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -4,21 +4,21 @@ using xadrez;
 
 try
 {
-    Tabuleiro tab = new Tabuleiro(8, 8);
+    PartidaDeXadrez partida = new PartidaDeXadrez();
 
-    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+    while (!partida.Terminada)
+    {
+        Console.Clear();
+        Tela.ImprimirTabuleiro(partida.Tab);
 
-    tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
-    //tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
-    //tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 2));
+        Console.Write("\nOrigem: ");
+        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+        Console.Write("Destino: ");
+        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
 
+        partida.ExecutaMovimento(origem, destino);
+    }
 
-
-    Tela.ImprimirTabuleiro(tab);
-
-    Console.ReadLine();
 
 }
 catch (TabuleiroException e)
