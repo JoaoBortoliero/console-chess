5f39f3541a7b139008fae12db928f8d0b1e9f486
João Vitor <bortoliero.10@hotmail.com>
2024-08-03 17:44:22 -0300

    Começando a implementar um metodo que imprimi o tabuleiro

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,6 +1,9 @@
-﻿using console_chess.tabuleiro;
+﻿using console_chess;
+using console_chess.tabuleiro;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+Tela.ImprimirTabuleiro(tab);
 
 Console.WriteLine("Tabuleiro: " + tab);
 Console.ReadLine();
---
+++ console-chess/Tela.cs
@@ -0,0 +1,23 @@
+﻿using console_chess.tabuleiro;
+
+namespace console_chess
+{
+    internal class Tela
+    {
+        public static void ImprimirTabuleiro(Tabuleiro tabuleiro)
+        {
+            for (int i = 0; i < tabuleiro.Linhas; i++)
+            {
+                for (int j = 0; j < tabuleiro.Colunas; j++)
+                {
+                    if (tabuleiro.Peca(i, j) == null)
+                        Console.Write("- ");
+                    else
+                        Console.Write(tabuleiro.Peca(i, j) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
